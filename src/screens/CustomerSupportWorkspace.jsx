c213a2e6e9654798e3f027f35b564c693cf69eaf
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-08-29 23:52:11 +0100

    Compose support centre with omnichannel extensions

---
+++ src/screens/CustomerSupportWorkspace.jsx
@@ -0,0 +1,9 @@
+import CustomerSupport from '@/screens/CustomerSupport';
+import SupportExtensions from '@/components/support/SupportExtensions';
+
+export default function CustomerSupportWorkspace() {
+  return <>
+    <CustomerSupport />
+    <SupportExtensions />
+  </>;
+}
