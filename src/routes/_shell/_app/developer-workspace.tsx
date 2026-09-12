350eab93405c9e10241ab97b2dbaf1ebff68cc89
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-12 22:47:53 +0100

    Add spatial redesign to blackstar-developer

--- src/routes/_shell/_app/developer-workspace.tsx
+++ src/routes/_shell/_app/developer-workspace.tsx
@@ -5,6 +5,10 @@ function SpatialPage() {
   return <div className="blackstar-core-page blackstar-secondary-page blackstar-developer"><Screen /></div>;
 }
 
+function SpatialPage() {
+  return <div className="blackstar-core-page blackstar-secondary-page blackstar-developer"><Screen /></div>;
+}
+
 export const Route = createFileRoute("/_shell/_app/developer-workspace")({
   head: () => ({
     meta: [
