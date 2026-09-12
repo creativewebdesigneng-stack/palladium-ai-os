115ea1242e6b7584ad2742b07c560ff9b3c426f3
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-12 22:47:55 +0100

    Add spatial redesign to blackstar-social

--- src/routes/_shell/_app/social-operations.tsx
+++ src/routes/_shell/_app/social-operations.tsx
@@ -5,6 +5,10 @@ function SpatialPage() {
   return <div className="blackstar-core-page blackstar-secondary-page blackstar-social"><Screen /></div>;
 }
 
+function SpatialPage() {
+  return <div className="blackstar-core-page blackstar-secondary-page blackstar-social"><Screen /></div>;
+}
+
 export const Route = createFileRoute("/_shell/_app/social-operations")({
   head: () => ({
     meta: [
