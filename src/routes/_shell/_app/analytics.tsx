e6c220660d3cabef67b09517dc8f8cd6031cae88
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-12 22:49:22 +0100

    Remove duplicate spatial wrapper declaration

--- src/routes/_shell/_app/analytics.tsx
+++ src/routes/_shell/_app/analytics.tsx
@@ -5,10 +5,6 @@ function SpatialPage() {
   return <div className="blackstar-core-page blackstar-secondary-page blackstar-analytics"><Screen /></div>;
 }
 
-function SpatialPage() {
-  return <div className="blackstar-core-page blackstar-secondary-page blackstar-analytics"><Screen /></div>;
-}
-
 export const Route = createFileRoute("/_shell/_app/analytics")({
   head: () => ({
     meta: [
