1fbbde9207a778905efb49be6a0ea957f3385ebd
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-12 22:47:28 +0100

    Add spatial redesign to blackstar-analytics

--- src/routes/_shell/_app/analytics.tsx
+++ src/routes/_shell/_app/analytics.tsx
@@ -5,6 +5,10 @@ function SpatialPage() {
   return <div className="blackstar-core-page blackstar-secondary-page blackstar-analytics"><Screen /></div>;
 }
 
+function SpatialPage() {
+  return <div className="blackstar-core-page blackstar-secondary-page blackstar-analytics"><Screen /></div>;
+}
+
 export const Route = createFileRoute("/_shell/_app/analytics")({
   head: () => ({
     meta: [
