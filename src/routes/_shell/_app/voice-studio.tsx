ed20735a7863146c20e5c4aa925b38a082d61ece
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-12 22:47:57 +0100

    Add spatial redesign to blackstar-voice

--- src/routes/_shell/_app/voice-studio.tsx
+++ src/routes/_shell/_app/voice-studio.tsx
@@ -1,6 +1,10 @@
 import { createFileRoute } from '@tanstack/react-router';
 import VoiceStudio from '@/screens/VoiceStudioWorkspace';
 
+function SpatialPage() {
+  return <div className="blackstar-core-page blackstar-secondary-page blackstar-voice"><VoiceStudio /></div>;
+}
+
 export const Route = createFileRoute('/_shell/_app/voice-studio')({
-  component: VoiceStudio,
+  component: SpatialPage,
 });
