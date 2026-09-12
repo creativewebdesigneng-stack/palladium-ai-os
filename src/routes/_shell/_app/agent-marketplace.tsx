de2141bd42161d6a2edbfb1b01431eda9b9c7e7f
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-12 23:02:11 +0100

    Remove duplicate long-tail spatial wrapper

--- src/routes/_shell/_app/agent-marketplace.tsx
+++ src/routes/_shell/_app/agent-marketplace.tsx
@@ -5,10 +5,6 @@ function SpatialPage() {
   return <div className="blackstar-core-page blackstar-longtail-page blackstar-agentmarket"><Screen /></div>;
 }
 
-function SpatialPage() {
-  return <div className="blackstar-core-page blackstar-longtail-page blackstar-agentmarket"><Screen /></div>;
-}
-
 export const Route = createFileRoute("/_shell/_app/agent-marketplace")({
   head: () => ({
     meta: [
