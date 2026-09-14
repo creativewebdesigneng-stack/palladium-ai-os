a5bc2cf058b56c5f18153086ca39fbd027593753
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-14 21:04:53 +0100

    Type Retail carrier server environment

---
+++ src/types/retail-provider-env.d.ts
@@ -0,0 +1,13 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    APP_ORIGIN?: string;
+    TWILIO_ACCOUNT_SID?: string;
+    TWILIO_AUTH_TOKEN?: string;
+    TWILIO_SMS_FROM_NUMBER?: string;
+    TWILIO_MESSAGING_SERVICE_SID?: string;
+    TWILIO_WHATSAPP_FROM?: string;
+    TWILIO_VOICE_FROM_NUMBER?: string;
+  }
+}
+
+export {};
