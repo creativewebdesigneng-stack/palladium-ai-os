740030f931c243a9f6d4460b8ea376c85aea0af0
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-26 13:34:09 +0100

    Add HUD corner ticks to CommandTheatre.

--- src/components/mission/CommandTheatre.jsx
+++ src/components/mission/CommandTheatre.jsx
@@ -1,6 +1,10 @@
 export default function CommandTheatre({ children }) {
   return (
     <div className="astra-command-theatre astra-room-mission">
+      <span aria-hidden className="astra-theatre-corner astra-theatre-corner-tl" />
+      <span aria-hidden className="astra-theatre-corner astra-theatre-corner-tr" />
+      <span aria-hidden className="astra-theatre-corner astra-theatre-corner-bl" />
+      <span aria-hidden className="astra-theatre-corner astra-theatre-corner-br" />
       {children}
     </div>
   );
