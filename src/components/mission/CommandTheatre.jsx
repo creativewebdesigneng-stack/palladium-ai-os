90576d0af5903a72d78d95b25035cfe856c45817
creativewebdesigneng-stack <creativewebdesigneng@gmail.com>
2026-09-26 18:37:18 +0100

    Caption the Mission Control theatre.

--- src/components/mission/CommandTheatre.jsx
+++ src/components/mission/CommandTheatre.jsx
@@ -5,6 +5,7 @@ export default function CommandTheatre({ children }) {
       <span aria-hidden className="astra-theatre-corner astra-theatre-corner-tr" />
       <span aria-hidden className="astra-theatre-corner astra-theatre-corner-bl" />
       <span aria-hidden className="astra-theatre-corner astra-theatre-corner-br" />
+      <p className="astra-room-label px-3 pt-2">Blackstar mission theatre</p>
       {children}
     </div>
   );
